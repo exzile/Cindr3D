e10aadf798e7b58743fbf137f2f4dedbe63c1a70
exzile <joeypongallo@gmail.com>
2026-05-05 10:17:53 -0400

    Capture active print session for resume

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './effects/autoSaveDzn';
+import './effects/printSessionResume';
 import { registerServiceWorker } from './pwa/registerServiceWorker';
 
 createRoot(document.getElementById('root')!).render(
